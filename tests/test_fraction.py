e475158cfce299a52cb240c26fbec75ea68b7fed
dhuberinbar <huberinbard@gmail.com>
2019-12-15 18:16:00 -0500

    added necessary imports

---
+++ tests/test_fraction.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import sysconfig
+import unittest
+import math
+import fraction
+import math
+import numbers
+import operator
+import re
+import sys
+
+from decimal import Decimal
+from test.support import requires_IEEE_754
+import ouroboros.fraction as fraction
+from ouroboros.fraction import *
+
+INF = float('inf')
+NAN = float('nan')
+NINF = float('-inf')
